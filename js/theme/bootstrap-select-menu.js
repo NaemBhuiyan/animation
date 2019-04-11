ff4898bef6e7f6afc72c5ddebfefcd39bb6552d7
Humayun kabir <humayunkabir.cep@gmail.com>
2019-04-11 17:15:39 +0600

    components

--- js/theme/bootstrap-select-menu.js
+++ js/theme/bootstrap-select-menu.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import utils from './Utils';
-import spDetector from './detector';
 
 /*-----------------------------------------------
 |   Select menu [bootstrap 4]
@@ -9,5 +8,5 @@ import spDetector from './detector';
 utils.$document.ready(() => {
   // https://getbootstrap.com/docs/4.0/getting-started/browsers-devices/#select-menu
   // https://github.com/twbs/bootstrap/issues/26183
-  spDetector.isAndroid && $('select.form-control').removeClass('form-control').css('width', '100%');
+  window.is.android() && $('select.form-control').removeClass('form-control').css('width', '100%');
 });
